cd7aa9d43db5d2ef3bbcbdad687e8f97914f0a53
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2024-05-01 18:50:22 -0700

    users now have profile picture urls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :image_url])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :image_url])
   end
 end
